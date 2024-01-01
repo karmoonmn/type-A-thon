47861d33aee1164fe421a3608d1b3e66e3e7838f
karmoonmn <153056819+karmoonmn@users.noreply.github.com>
2024-01-01 22:29:01 +0800

    Delete type/src directory

--- type/src/LoginAndSignUp.java
+++
@@ -1,7 +0,0 @@
-public class LoginAndSignUp {
-    public static void main(String[] args) {
-        Profile LoginFrame = new Profile();
-        LoginFrame.setVisible(true);
-        LoginFrame.setLocationRelativeTo(null);
-    }
-}
