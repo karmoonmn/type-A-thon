8987ca0a1c260b733801ac7fce9f25288dfcd2cb
karmoonmn <153056819+karmoonmn@users.noreply.github.com>
2024-01-01 22:58:58 +0800

    Update MainGame.java

--- type/src/MainGame.java
+++ type/src/MainGame.java
@@ -37,34 +37,4 @@ public class MainGame extends JFrame {
     }
 
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please choose the gamemode : ");
-        int gamemode = scanner.nextInt();
-        scanner.nextLine();
-
-
-        SwingUtilities.invokeLater(() -> {
-            switch (gamemode) {
-                case 0: //quickstart
-                    new MainGame(50, "dictionary.txt", 10, "Default Mode").setVisible(true);
-                    break;
-                case 1: //can edit timer, choose yes or no punctuation
-                    new MainGame(30, "dictionary2.txt", 50, "Timed Mode").setVisible(true);
-                    break;
-                case 2: //stopwatch, choose num of word
-                    new MainGame(0, "dictionary.txt", 100, true).setVisible(true);
-                    break;
-                case 3: //quote
-                    new MainGame(30, "quote.txt", 5, "Quotes Mode").setVisible(true);
-                    break;
-                case 4: //sudden death
-                    new MainGame(30, "dictionary.txt", 40, 3).setVisible(true);
-                default:
-                    System.out.println();
-            }
-
-
-        });
-    }
 }
